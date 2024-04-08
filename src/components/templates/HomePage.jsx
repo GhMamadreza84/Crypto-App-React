f58ef763ef3afd52d30618ed316f61d9b8fe54c3
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-04-08 18:33:00 +0200

    fetch succesfully

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,9 +1,13 @@
 import { useEffect } from "react";
 
 const HomePage = () => {
-    useEffect(()=>{
-
-    },[])
-    return (<div>Home Page</div>);
-}
+  useEffect(() => {
+    fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&&x_cg_demo_api_key=CG-fE2xUGccdiFXM9THzncMJfDH"
+    )
+      .then((res) => res.json())
+      .then((json) => console.log(json));
+  }, []);
+  return <div>Home Page</div>;
+};
 export default HomePage;
