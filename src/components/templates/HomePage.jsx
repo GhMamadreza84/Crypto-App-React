0839bf65274f47494f2088dd6ac24fa89a5ed7d8
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-04-08 18:33:45 +0200

    create coins state

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,6 +1,7 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const HomePage = () => {
+  const [coins, setCoins] = useState([]);
   useEffect(() => {
     fetch(
       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&&x_cg_demo_api_key=CG-fE2xUGccdiFXM9THzncMJfDH"
