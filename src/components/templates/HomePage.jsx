8d89c42048900dfb447bbef0d3ca1a98e18af8ad
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-04-08 18:35:25 +0200

    import tablecoin in home and pass state with props

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-
+import TableCoin from "../modules/TableCoin";
 const HomePage = () => {
   const [coins, setCoins] = useState([]);
   useEffect(() => {
@@ -7,8 +7,12 @@ const HomePage = () => {
       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&&x_cg_demo_api_key=CG-fE2xUGccdiFXM9THzncMJfDH"
     )
       .then((res) => res.json())
-      .then((json) => console.log(json));
+      .then((json) => setCoins(json));
   }, []);
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <TableCoin />
+    </div>
+  );
 };
 export default HomePage;
