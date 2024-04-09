fe202feea7511c737afcd58669a114bcd27ccdf1
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-04-09 16:20:39 +0200

    edit main js

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './global.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./global.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   //<React.StrictMode>
-    <App />
+  <App />
   //</React.StrictMode>,
-)
+);
+                                                      const aboutMe = {
+                                                        iAm: "Front End Developer",
+                                                        iLove: ["Challenge", "Sport", "Game"],
+                                                        iWorkWith: ["React", "Js", "TaliwindCss"],
+                                                      };
