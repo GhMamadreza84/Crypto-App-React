7852167b11df59fabe31b9d1af9a8bf274c3c96b
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-04-10 09:42:41 +0200

    edit BASE_URL and API_KEY in cryptoList

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,4 +1,6 @@
+const BASE_URL = "https://api.coingecko.com/api/v3";
+const API_KEY = "CG-fE2xUGccdiFXM9THzncMJfDH";
 const getCoinList = () => {
-  return "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&&x_cg_demo_api_key=CG-fE2xUGccdiFXM9THzncMJfDH";
+  return `${BASE_URL}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&&x_cg_demo_api_key=${API_KEY}`;
 };
-export  { getCoinList };
+export { getCoinList };
