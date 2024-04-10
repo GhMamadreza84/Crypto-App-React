86bf234b408d2c82781b36fd97ea1bdb8a6a36d0
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-04-10 10:01:53 +0200

    add first trow in tbody table in tableCoin.jsx

--- src/components/modules/Tablecoin.jsx
+++ src/components/modules/Tablecoin.jsx
@@ -1,6 +1,33 @@
 const TableCoin = ({ coins }) => {
   console.log(coins);
-  return <div>TableCoins</div>;
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Coin</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>24h</th>
+            <th>Total Volume</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {coins.map((coin) => (
+            <tr key={coin.id}>
+              <td>
+                <div>
+                  <img src={coin.image} alt="" />
+                  <span>{coin.symbol.toUpperCase()}</span>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default TableCoin;
