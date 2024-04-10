396eab849e4f464b718ff2a35ca0ed47a6b353fb
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-04-10 10:40:24 +0200

    remove errors files in tablecoin and get back again and edit this

--- src/components/modules/Tablecoin.jsx
+++ src/components/modules/Tablecoin.jsx
@@ -25,34 +25,3 @@ const TableCoin = ({ coins }) => {
 };
 
 export default TableCoin;
-const TableRow = ({
-  coin: {
-    name,
-    image,
-    symbol,
-    current_price,
-    current_change_percentage_24h :price_change,
-    total_volume,
-  },
-}) => {
-  return (
-    <tr>
-      <td>
-        <div>
-          <img src={image} alt={name} />
-          <span>{symbol.toUpperCase()}</span>
-        </div>
-      </td>
-      <td>{name}</td>
-      <td>${current_price.toLocaleString()}</td>
-      <td>{price_change.toFixed(2)}</td>
-      <td>{total_volume.toLocaleString()}</td>
-      <td>
-        <img
-          src={price_change > 0 ? chartUp : chartDown}
-          alt={name}
-        />
-      </td>
-    </tr>
-  );
-};
