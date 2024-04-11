979a8b104867e58c2e3cfb98e888238a8e3f0ca6
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-04-11 19:06:19 +0200

    create onclick function previous and next handler

--- src/components/modules/Pagination.jsx
+++ src/components/modules/Pagination.jsx
@@ -1,14 +1,20 @@
-import {useState} from "react"
+import { useState } from "react";
 
 const Pagination = () => {
-  const [page,setPage]=useState(1)
+  const [page, setPage] = useState(1);
+  const previousHandler = () => {
+    setPage((page) => page - 1);
+  };
+  const nextHandler = () => {
+    setPage((page) => page + 1);
+  };
   return (
     <div>
-      <button>Previous</button>
-      <p></p>
-      <button>Previous</button>
+      <button onClick={previousHandler}>Previous</button>
+      <p>{page}</p>
+      <button onClick={nextHandler}>Next</button>
     </div>
-  )
+  );
 };
 
 export default Pagination;
