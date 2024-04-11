051c333d4a7eb3b5adb9f1c474a8ad2d0c239f82
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-04-11 18:58:21 +0200

    import and show in homepage ui

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
-import TableCoin from "../modules/TableCoin";
 import { getCoinList } from "../../services/cryptoApi";
+import TableCoin from "../modules/TableCoin";
+import Pagination from "../modules/Pagination";
 const HomePage = () => {
   const [coins, setCoins] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
