9645b7dc1b8f0085ef531c3eab4c4cde13735274
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-04-11 19:53:48 +0200

    add setCurrency to project you can watch now usd , eur , jpy currency

--- src/components/modules/Search.jsx
+++ src/components/modules/Search.jsx
@@ -1,9 +1,14 @@
-
-const Search = () => {
+const Search = ({ currency, setCurrency }) => {
   return (
     <div>
+      <input type="text" />
+      <select value={currency} onChange={(e) =>setCurrency( e.target.value)}>
+        <option value="usd">USD</option>
+        <option value="eur">EUR</option>
+        <option value="jpy">JPY</option>
+      </select>
     </div>
   );
-}
+};
 
 export default Search;
--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -2,23 +2,26 @@ import { useEffect, useState } from "react";
 import { getCoinList } from "../../services/cryptoApi";
 import TableCoin from "../modules/TableCoin";
 import Pagination from "../modules/Pagination";
+import Search from "../modules/Search";
 const HomePage = () => {
   const [coins, setCoins] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [page, setPage] = useState(1);
+  const [currency, setCurrency] = useState("usd");
   useEffect(() => {
     const getData = async () => {
-      const res = await fetch(getCoinList(page));
-      setIsLoading(true)
+      const res = await fetch(getCoinList(page, currency));
+      setIsLoading(true);
       const data = await res.json();
       setCoins(data);
       setIsLoading(false);
     };
     getData();
-  }, [page]);
+  }, [page,currency]);
   return (
     <div>
-      <TableCoin coins={coins} isLoading={isLoading}  />
+      <Search currency={currency} setCurrency={setCurrency} />
+      <TableCoin coins={coins} isLoading={isLoading} />
       <Pagination page={page} setPage={setPage} />
     </div>
   );
