01891e097247070790db81bec16e439514836f0e
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-04-11 20:03:45 +0200

    edit per page to 10

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,6 +1,6 @@
 const BASE_URL = "https://api.coingecko.com/api/v3";
 const API_KEY = "CG-fE2xUGccdiFXM9THzncMJfDH";
 const getCoinList = (page,currency) => {
-  return `${BASE_URL}/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=20&page=${page}&&x_cg_demo_api_key=${API_KEY}`;
+  return `${BASE_URL}/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=10&page=${page}&&x_cg_demo_api_key=${API_KEY}`;
 };
 export { getCoinList };
