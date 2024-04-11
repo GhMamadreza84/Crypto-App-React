704b8e2340c4c5c268537650db4fd1e3f0fcb80e
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-04-11 19:07:52 +0200

    handle click when page count is <1 return function

--- src/components/modules/Pagination.jsx
+++ src/components/modules/Pagination.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 const Pagination = () => {
   const [page, setPage] = useState(1);
   const previousHandler = () => {
+    if (page <= 1) return;
     setPage((page) => page - 1);
   };
   const nextHandler = () => {
