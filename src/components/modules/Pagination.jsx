93d0ca23f04ede4f7b5b0f55490515e231c9c039
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-04-11 19:17:01 +0200

    remove inline state

--- src/components/modules/Pagination.jsx
+++ src/components/modules/Pagination.jsx
@@ -13,8 +13,8 @@ const Pagination = () => {
   return (
     <div>
       <button onClick={previousHandler}>Previous</button>
-      <p style={{ color: page === 1 ? "red" : "inherit" }}>1</p>
-      <p style={{ color: page === 2 ? "red" : "inherit" }}>2</p>
+      <p >1</p>
+      <p >2</p>
       {page > 2 && page < 9 && (
         <>
           <span>...</span>
