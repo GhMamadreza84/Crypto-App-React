447e772c72235550e6846a55bd50f4e310bc14d1
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-04-12 17:37:28 +0200

    import useEffect

--- src/components/modules/Search.jsx
+++ src/components/modules/Search.jsx
@@ -1,10 +1,18 @@
-import {useState} from "react"
+import { useEffect, useState } from "react";
 const Search = ({ currency, setCurrency }) => {
-  const [text,setText]=useState("")
+  const [text, setText] = useState("");
+  useEffect(()=>{
+    console.log(text);
+  },[text])
   return (
     <div>
-      <input type="text" />
-      <select value={currency} onChange={(e) =>setCurrency( e.target.value)}>
+      <input
+        type="text"
+        placeholder="Search"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <select value={currency} onChange={(e) => setCurrency(e.target.value)}>
         <option value="usd">USD</option>
         <option value="eur">EUR</option>
         <option value="jpy">JPY</option>
