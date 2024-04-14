ffccd52e82561136fd6e5e75bb723ef4846baef2
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-04-14 20:13:46 +0200

    edit tablecoin

--- src/components/modules/Tablecoin.jsx
+++ src/components/modules/Tablecoin.jsx
@@ -40,6 +40,7 @@ export default TableCoin;
 
 const TableRow = ({
   coin: {
+    id,
     name,
     image,
     current_price,
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -3,10 +3,10 @@ const API_KEY = "CG-fE2xUGccdiFXM9THzncMJfDH";
 const getCoinList = (page, currency) => {
   return `${BASE_URL}/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=10&page=${page}&&x_cg_demo_api_key=${API_KEY}`;
 };
-const searchCoin = (query)=>{
-  return `${BASE_URL}/search?query=${query}&x_cg_demo_api_key=${API_KEY}`
-}
-const marketChart = coin=>{
-  return `${BASE_URL}/coins/${coin}/market_chart?vs_currency?days=7&x_cg_demo_api_key=${API_KEY}`
-}
-export { getCoinList,searchCoin };
+const searchCoin = (query) => {
+  return `${BASE_URL}/search?query=${query}&x_cg_demo_api_key=${API_KEY}`;
+};
+const marketChart = (coin) => {
+  return `${BASE_URL}/coins/${coin}/market_chart?vs_currency?days=7&x_cg_demo_api_key=${API_KEY}`;
+};
+export { getCoinList, searchCoin, marketChart };
