4aa7aaaba609b597b5e53b4eb3a37eb67f1de17d
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-04-14 18:49:00 +0200

    change icon in chart component

--- src/components/modules/Chart.jsx
+++ src/components/modules/Chart.jsx
@@ -1,9 +1,9 @@
-import { IoMdClose } from "react-icons/io";
+import { IoClose } from "react-icons/io5";
 import styles from "./Chart.module.css"
 const Chart = ({chart,setChart}) => {
   return (
     <div className={styles.container}>
-     <span onClick={()=>setChart(null)} className={styles.cross}><IoMdClose /></span>
+     <span onClick={()=>setChart(null)} className={styles.cross}><IoClose /></span>
     </div>
   );
 }
