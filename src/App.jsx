1a6e3c39da2929be26d662a9ecb5e6ec0ee750c1
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-04-29 16:46:53 +0200

    import home page in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import ChartPage from "./components/templates/ChartPage"
+import HomePage from "./components/templates/ChartPage";
+import ChartPage from "./components/templates/ChartPage";
 
 function App() {
   return (
     <>
+      <HomePage />
       <ChartPage />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
