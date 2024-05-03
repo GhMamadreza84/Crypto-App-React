183df0d471930c86cff6dbb6003750f9471800f3
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-03 18:52:20 +0200

    add second page to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import HomePage from "./components/templates/HomePage";
 import ChartPage from "./components/templates/ChartPage";
+import SecondPage from "./components/templates/SecondPage";
 
 function App() {
   return (
     <>
       <HomePage />
+      <SecondPage />
       <ChartPage />
     </>
   );
---
+++ src/components/templates/SecondPage.jsx
@@ -0,0 +1,10 @@
+import styles from "./SecondPage.module.css"
+const SecondPage = () => {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  );
+}
+
+export default SecondPage;
