7f4025b97dd0c8cc6a1dc542ca2e84b11a0e96cb
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-03 19:45:53 +0200

    import icons

--- src/components/templates/SecondPage.jsx
+++ src/components/templates/SecondPage.jsx
@@ -1,3 +1,5 @@
+import { FaArrowRight } from "react-icons/fa6";
+
 import styles from "./SecondPage.module.css";
 import tradeImage from "../../assets/trade.png";
 import atmImage from "../../assets/atm.png";
@@ -11,30 +13,36 @@ const SecondPage = () => {
       <div className={styles.containerDiv}>
         <div>
           <img src={tradeImage} alt="tradeImage" />
-          <h2>Trade Desk</h2>
-          <p>
-            Invest in crypto anytime, anywhere with our safe, secure, and easy
-            to use online platform
-          </p>
-          <a href="#">Get Started</a>
+          <div className={styles.title}>
+            <h2>Trade Desk</h2>
+            <p>
+              Invest in crypto anytime, anywhere with our safe, secure, and easy
+              to use online platform
+            </p>
+            <a href="#">Get Started</a>
+          </div>
         </div>
         <div>
           <img src={atmImage} alt="atmImage" />
-          <h2>CoinFlip ATMs</h2>
-          <p>
-            We have thousands of ATMs located across the U.S. where you can
-            easily convert cash to crypto
-          </p>
-          <a href="#">Find an ATM</a>
+          <div className={styles.title}>
+            <h2>CoinFlip ATMs</h2>
+            <p>
+              We have thousands of ATMs located across the U.S. where you can
+              easily convert cash to crypto
+            </p>
+            <a href="#">Find an ATM</a>
+          </div>
         </div>
         <div>
           <img src={walletImage} alt="walletImage" />
-          <h2>CoinFlip Wallet</h2>
-          <p>
-            Store your growing investments in our non-custodial wallet that
-            gives you access to a full suite of DeFi services in one place
-          </p>
-          <a href="#">Download the App</a>
+          <div className={styles.title}>
+            <h2>CoinFlip Wallet</h2>
+            <p>
+              Store your growing investments in our non-custodial wallet that
+              gives you access to a full suite of DeFi services in one place
+            </p>
+            <a href="#">Download the App</a>
+          </div>
         </div>
       </div>
       <img src={star1} alt="star1" />
