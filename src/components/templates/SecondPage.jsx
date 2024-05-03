21ec286c0d6eab3eadcc53645de15ddac233c92a
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-03 19:59:49 +0200

    edit divs

--- src/components/templates/SecondPage.jsx
+++ src/components/templates/SecondPage.jsx
@@ -13,36 +13,36 @@ const SecondPage = () => {
       <div className={styles.containerDiv}>
         <div>
           <img src={tradeImage} alt="tradeImage" />
-          <section className={styles.title}>
+          <div className={styles.title}>
             <h2>Trade Desk</h2>
             <p>
               Invest in crypto anytime, anywhere with our safe, secure, and easy
               to use online platform
             </p>
             <a href="#">Get Started <FaArrowRight /></a>
-          </section>
+          </div>
         </div>
         <div>
           <img src={atmImage} alt="atmImage" />
-          <section className={styles.title}>
+          <div className={styles.title}>
             <h2>CoinFlip ATMs</h2>
             <p>
               We have thousands of ATMs located across the U.S. where you can
               easily convert cash to crypto
             </p>
             <a href="#">Find an ATM <FaArrowRight /></a>
-          </section>
+          </div>
         </div>
         <div>
           <img src={walletImage} alt="walletImage" />
-          <section className={styles.title}>
+          <div className={styles.title}>
             <h2>CoinFlip Wallet</h2>
             <p>
               Store your growing investments in our non-custodial wallet that
               gives you access to a full suite of DeFi services in one place
             </p>
-            <a href="#">Download the App <FaArrowRight  /></a>
-          </section>
+            <a href="#">Download the App <FaArrowRight /> </a>
+          </div>
         </div>
       </div>
       <img src={star1} alt="star1" />
