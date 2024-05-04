e488d803142f7785afeea262102664242923ac73
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-04 19:03:44 +0200

    edit classnames

--- src/components/templates/SupportPage.jsx
+++ src/components/templates/SupportPage.jsx
@@ -1,6 +1,11 @@
 import styles from "./SupportPage.module.css";
 
 import StartBtn from "../modules/StartBtn";
+
+import orangeGradient from "../../assets/orangeGradient.png";
+import sphereOrange from "../../assets/sphereOrange.png";
+import userIcon from "../../assets/user-icon.png";
+import star2 from "../../assets/star-img2.png";
 const SupportPage = () => {
   return (
     <div className={styles.container}>
@@ -12,7 +17,15 @@ const SupportPage = () => {
         </p>
         <StartBtn />
       </div>
-      <div className={styles.section2}></div>
+      <div className={styles.section2}>
+        <div className={styles.images}>
+          <img src={orangeGradient} alt="orangeGradient"  className={styles.img1}/>
+          <img src={sphereOrange} alt="sphereOrange" className={styles.img2}/>
+          <img src={userIcon} alt="userIcon" className={styles.img3}/>
+          <img src={star2} alt="star2" className={styles.img4}/>
+        </div>
+        <div className={styles.title}></div>
+      </div>
     </div>
   );
 };
