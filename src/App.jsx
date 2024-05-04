a1b4aa4a0c7eb6249f65376cab85ee4b532c2508
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-04 18:31:51 +0200

    show support page in ui by used in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import HomePage from "./components/templates/HomePage";
 import ChartPage from "./components/templates/ChartPage";
 import SecondPage from "./components/templates/SecondPage";
+import SupportPage from "./components/templates/SupportPage";
 
 function App() {
   return (
     <>
       <HomePage />
       <SecondPage />
+      <SupportPage />
       <ChartPage />
     </>
   );
