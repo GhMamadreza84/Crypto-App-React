9270d9c91d62d731ef9f3ae55e6553ea68f6a5f0
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-04 18:38:58 +0200

    add section 1 text in support page

--- src/components/templates/SupportPage.jsx
+++ src/components/templates/SupportPage.jsx
@@ -1,8 +1,17 @@
 import styles from "./SupportPage.module.css";
+
+import StartBtn from "../modules/StartBtn";
 const SupportPage = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.section1}></div>
+      <div className={styles.section1}>
+        <h2>A crypto investment platform that invests in you </h2>
+        <p>
+          We invest more resources than any other platrom in making sure great
+          support from real people is a click away, whenever you need it.
+        </p>
+        <StartBtn />
+      </div>
       <div className={styles.section2}></div>
     </div>
   );
