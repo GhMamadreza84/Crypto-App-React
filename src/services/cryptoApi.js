73a54be93587300b129047ffad082fc9a86ab151
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-05 17:14:37 +0200

    edit api  fetch link

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,7 +1,7 @@
 const BASE_URL = "https://api.coingecko.com/api/v3";
 const API_KEY = "CG-fE2xUGccdiFXM9THzncMJfDH";
 const getCoinList = (page, currency) => {
-  return `${BASE_URL}/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=10&page=${page}&&x_cg_demo_api_key=${API_KEY}`;
+  return `${BASE_URL}/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=5&page=${page}&&x_cg_demo_api_key=${API_KEY}`;
 };
 const searchCoin = (query) => {
   return `${BASE_URL}/search?query=${query}&x_cg_demo_api_key=${API_KEY}`;
