e86cf28afd399a3c99bb636645891ee640410415
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-05 16:18:31 +0200

    remove search bar

--- src/components/templates/ChartPage.jsx
+++ src/components/templates/ChartPage.jsx
@@ -29,7 +29,7 @@ const HomePage = () => {
   }, [page, currency]);
   return (
     <div className={styles.container}>
-      <Search currency={currency} setCurrency={setCurrency} />
+      {/* <Search currency={currency} setCurrency={setCurrency} /> */}
       <TableCoin coins={coins} isLoading={isLoading} currency={currency} setChart={setChart}/>
       <Pagination page={page} setPage={setPage} />
       {!!chart && <Chart chart={chart} setChart={setChart} />}
