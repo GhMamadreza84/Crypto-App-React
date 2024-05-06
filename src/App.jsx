dc5602d5121e2f59aae35e319d74ed037b05c616
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-06 21:41:31 +0200

    import invest page in app and show in ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import HomePage from "./components/templates/HomePage";
 import ChartPage from "./components/templates/ChartPage";
 import SecondPage from "./components/templates/SecondPage";
 import SupportPage from "./components/templates/SupportPage";
+import InvestPage from "./components/templates/InvestPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <SecondPage />
       <SupportPage />
       <ChartPage />
+      <InvestPage />
     </>
   );
 }
