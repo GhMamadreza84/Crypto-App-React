4e5206a9f3e21773494fa13a6579d4ce3b9daa98
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-07 20:43:45 +0200

    start create feedback divs in footer

--- src/components/templates/Footer.jsx
+++ src/components/templates/Footer.jsx
@@ -2,7 +2,9 @@ import styles from "./Footer.module.css";
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.feedBack}></div>
+      <div className={styles.feedBack}>
+        
+      </div>
       <div className={styles.footerContent}></div>
     </div>
   );
