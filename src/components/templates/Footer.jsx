ca5e78fc53a263f622be17c16f8d413008240065
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-07 20:43:20 +0200

    create divs in footer

--- src/components/templates/Footer.jsx
+++ src/components/templates/Footer.jsx
@@ -1,9 +1,11 @@
-import styles from "./Footer.module.css"
+import styles from "./Footer.module.css";
 const Footer = () => {
   return (
-    <div >
+    <div className={styles.container}>
+      <div className={styles.feedBack}></div>
+      <div className={styles.footerContent}></div>
     </div>
   );
-}
+};
 
 export default Footer;
