8fd73369cca5de350dc886b81e78f608baf24d40
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-08 00:02:32 +0200

    add ul to center Div

--- src/components/templates/Footer.jsx
+++ src/components/templates/Footer.jsx
@@ -13,12 +13,39 @@ const Footer = () => {
       <div className={styles.footerContent}>
         <div className={styles.leftDiv}>
           <h1>COINFLIP</h1>
-          <p>CoinFlip, the world's leading bitcoin ATM operator, makes it so flippin' easy to buy and sell bitcoin via cash, card, or bank transfer. </p>
-          <p>Sign up to get the latest in CoinFlip news, discounts, and more.</p>
+          <p>
+            CoinFlip, the world's leading bitcoin ATM operator, makes it so
+            flippin' easy to buy and sell bitcoin via cash, card, or bank
+            transfer.{" "}
+          </p>
+          <p>
+            Sign up to get the latest in CoinFlip news, discounts, and more.
+          </p>
           <Input />
           <p>© 2024 Created With 💙 By Gh_Mamadreza</p>
         </div>
-        <div  className={styles.centerDiv}></div>
+        <div className={styles.centerDiv}>
+          <ul>
+            <li>
+              <h1>Company</h1>
+            </li>
+            <li>
+              <p>About</p>
+            </li>
+            <li>
+              <p>Careers</p>
+            </li>
+            <li>
+              <p>Press</p>
+            </li>
+            <li>
+              <p>News</p>
+            </li>
+            <li>
+              <p>Merch</p>
+            </li>
+          </ul>
+        </div>
         <div className={styles.rightDiv}></div>
       </div>
     </div>
