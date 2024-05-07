d15533ff2222e9a0d6a94b7c840793ce368ba027
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-07 20:45:21 +0200

    create content feedback div

--- src/components/templates/Footer.jsx
+++ src/components/templates/Footer.jsx
@@ -3,7 +3,9 @@ const Footer = () => {
   return (
     <div className={styles.container}>
       <div className={styles.feedBack}>
-        
+        <h1></h1>
+        <p></p>
+        <input type="email" placeholder="Email Address" />
       </div>
       <div className={styles.footerContent}></div>
     </div>
