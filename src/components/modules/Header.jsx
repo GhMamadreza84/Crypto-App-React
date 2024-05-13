a2ab44a71e30809747af88870e2e4cac851218df
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-13 17:41:54 +0200

    edit styles

--- src/components/modules/Header.jsx
+++ src/components/modules/Header.jsx
@@ -18,7 +18,22 @@ const Header = () => {
         <button className={styles.login}>Sign in</button>
         <button className={styles.signup}>Sign up</button>
       </div>
-      
+      <div className={styles.burgerBtn}></div>
+      <div className={styles.burgerMenu}>
+        <div className={styles.buttons}>
+          <button className={styles.login}>Sign in</button>
+          <button className={styles.signup}>Sign up</button>
+        </div>
+        <div className={styles.menu}>
+          <ul className={styles.list}>
+            <li>Buy/Sell</li>
+            <li>Grow</li>
+            <li>Market</li>
+            <li>Business</li>
+            <li>Support</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
