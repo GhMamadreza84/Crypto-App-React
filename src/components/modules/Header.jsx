e9ba781a24b67fa0217e246eef5604ee29c62b06
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-14 16:35:09 +0200

    edit styles

--- src/components/modules/Header.jsx
+++ src/components/modules/Header.jsx
@@ -6,9 +6,13 @@ import { IoMenu } from "react-icons/io5";
 import styles from "./Header.module.css";
 const Header = () => {
   const [burgerOpen, setBurgerOpen] = useState(false);
-  const menuHandler = () => {
+  const burgerOpenHandler = () => {
     setBurgerOpen(!burgerOpen);
   };
+  const burgerCloseHandler = () => {
+    setBurgerOpen(!burgerOpen);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.logo}>
@@ -28,29 +32,33 @@ const Header = () => {
         <button className={styles.signup}>Sign up</button>
       </div>
 
-      <div className={styles.openBurger} onClick={menuHandler}>
-        <IoMenu />
+      <div className={styles.openBurger} onClick={burgerOpenHandler}>
+        {burgerOpen ? "" : <IoMenu />}
       </div>
 
-      <div className={styles.burgerMenu}>
-        <div className={styles.closeBurger}>
-          <IoMdClose className={styles.closeBurgerBtn} />
-        </div>
+      {burgerOpen ? (
+        <div className={styles.burgerMenu}>
+          <div className={styles.closeBurger} onClick={burgerCloseHandler}>
+            <IoMdClose className={styles.closeBurgerBtn} />
+          </div>
 
-        <div className={styles.menuList}>
-          <ul className={styles.list}>
-            <li>Buy/Sell</li>
-            <li>Grow</li>
-            <li>Market</li>
-            <li>Business</li>
-            <li>Support</li>
-          </ul>
+          <div className={styles.menuList}>
+            <ul className={styles.list}>
+              <li>Buy/Sell</li>
+              <li>Grow</li>
+              <li>Market</li>
+              <li>Business</li>
+              <li>Support</li>
+            </ul>
+          </div>
+          <div className={styles.burgerButtons}>
+            <button className={styles.login}>Sign in</button>
+            <button className={styles.signup}>Sign up</button>
+          </div>
         </div>
-        <div className={styles.burgerButtons}>
-          <button className={styles.login}>Sign in</button>
-          <button className={styles.signup}>Sign up</button>
-        </div>
-      </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
