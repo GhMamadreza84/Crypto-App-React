b178ad738ff4567ceb1d01520374aa6bdf67aee0
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-14 15:58:13 +0200

    use new icon

--- src/components/modules/Header.jsx
+++ src/components/modules/Header.jsx
@@ -26,7 +26,7 @@ const Header = () => {
       </div>
 
       <div className={styles.openBurger }>
-      <CiMenuFries />
+      <IoMenu />
       </div>
 
       <div className={styles.burgerMenu}>
